ff26cab146e4738050975ab5169fd3661bea0550
Stefankante <35141284+Stefankante@users.noreply.github.com>
2023-09-14 09:56:36 +0700

    Add files via upload

---
+++ WT32-SC01Plus_LVGL/src/WT32-SC01Plus_LVGL.ino
@@ -0,0 +1,174 @@
+#include <Wire.h>
+#include <lvgl.h>
+#define LGFX_USE_V1
+#include <demos/lv_demos.h>
+#include <LovyanGFX.hpp>
+#include <SimpleTimer.h>
+#include <Arduino_GFX_Library.h>
+
+#define GFX_BL 45
+Arduino_DataBus *bus = new Arduino_ESP32LCD8(
+    0 /* DC */, GFX_NOT_DEFINED /* CS */, 47 /* WR */, GFX_NOT_DEFINED /* RD */,
+    9 /* D0 */, 46 /* D1 */, 3 /* D2 */, 8 /* D3 */, 18 /* D4 */, 17 /* D5 */, 16 /* D6 */, 15 /* D7 */);
+Arduino_GFX *gfx = new Arduino_ST7796(bus, 4 /* RST */, 0 /* rotation */, true /* IPS */);
+
+class LGFX : public lgfx::LGFX_Device
+{
+  lgfx::Panel_ST7796 _panel_instance;
+  lgfx::Bus_Parallel8 _bus_instance;
+  lgfx::Touch_FT5x06 _touch_instance;
+
+public:
+  LGFX(void)
+  {
+
+
+    { // Configure settings for touch screen control. (delete if not necessary)
+      auto cfg = _touch_instance.config();
+
+      cfg.x_min = 0;   // Minimum X value (raw value) obtained from the touchscreen
+      cfg.x_max = 319; // Maximum X value (raw value) obtained from the touchscreen
+      cfg.y_min = 0;   // Minimum Y value obtained from touchscreen (raw value)
+      cfg.y_max = 479; // Maximum Y value (raw value) obtained from the touchscreen
+      cfg.pin_int = 7; // pin number to which INT is connected
+      cfg.bus_shared = false;
+      cfg.offset_rotation = 0;
+
+      // For I2C connection
+      cfg.i2c_port = 0;    // Select I2C to use (0 or 1)
+      cfg.i2c_addr = 0x38; // I2C device address number
+      cfg.pin_sda = 6;     // pin number where SDA is connected
+      cfg.pin_scl = 5;     // pin number to which SCL is connected
+      cfg.freq = 400000;   // set I2C clock
+
+      _touch_instance.config(cfg);
+      _panel_instance.setTouch(&_touch_instance); // Set the touchscreen to the panel.
+    }
+
+    setPanel(&_panel_instance); // Sets the panel to use.
+  }
+};
+
+LGFX tft;
+
+/* Change to your screen resolution */
+static uint32_t screenWidth;
+static uint32_t screenHeight;
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t *disp_draw_buf;
+static lv_disp_drv_t disp_drv;
+
+
+/* Display flushing */
+void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
+{
+  uint32_t w = (area->x2 - area->x1 + 1);
+  uint32_t h = (area->y2 - area->y1 + 1);
+
+#if (LV_COLOR_16_SWAP != 0)
+  gfx->draw16bitBeRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
+#else
+  gfx->draw16bitRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
+#endif
+
+  lv_disp_flush_ready(disp);
+}
+
+void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
+{
+  uint16_t touchX, touchY;
+
+  bool touched = tft.getTouch(&touchX, &touchY);
+
+  if (!touched)
+  {
+      
+    data->state = LV_INDEV_STATE_REL;
+  }
+  else
+  {
+    data->state = LV_INDEV_STATE_PR;
+    /*Set the coordinates*/
+    data->point.x = touchX;
+    data->point.y = touchY;
+  }
+
+}
+
+
+void setup()
+{
+  
+  Serial.begin(115200);
+  // while (!Serial);
+  Serial.println("LVGL Widgets Demo");
+  //I2Ctwo.begin(21, 22, 100000);
+   pinMode(GFX_BL, OUTPUT);
+  digitalWrite(GFX_BL,HIGH);
+
+   int Rotation = 1;
+
+tft.setRotation(Rotation); ////// Initialize LOVYANGFX Touch
+tft.init();
+
+gfx->setRotation(Rotation);
+gfx->begin(40000000);
+gfx->fillScreen(BLACK);
+
+
+  lv_init();
+  delay(20);
+
+  screenWidth = gfx->width();
+  screenHeight = gfx->height();
+
+#ifdef ESP32
+  disp_draw_buf = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * screenWidth * screenHeight/2 , MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+#else
+  disp_draw_buf = (lv_color_t *)malloc(sizeof(lv_color_t) * screenWidth * screenHeight/2);
+#endif
+  if (!disp_draw_buf)
+  {
+    Serial.println("LVGL disp_draw_buf allocate failed!");
+  }
+  else
+  {
+    lv_disp_draw_buf_init(&draw_buf, disp_draw_buf, NULL, screenWidth * screenHeight/2);
+
+    /* Initialize the display */
+    lv_disp_drv_init(&disp_drv);
+    /* Change the following line to your display resolution */
+    disp_drv.hor_res = screenWidth;
+    disp_drv.ver_res = screenHeight;
+    disp_drv.flush_cb = my_disp_flush;
+    disp_drv.draw_buf = &draw_buf;
+    lv_disp_drv_register(&disp_drv);
+
+//     /* Initialize the (dummy) input device driver */
+    /* Initialize the input device driver */
+    static lv_indev_drv_t indev_drv;
+    lv_indev_drv_init(&indev_drv);
+    indev_drv.type = LV_INDEV_TYPE_POINTER;
+    indev_drv.read_cb = my_touchpad_read;
+    lv_indev_drv_register(&indev_drv);
+
+
+
+
+
+lv_demo_widgets();  
+//lv_demo_benchmark();
+
+    Serial.println("Setup done");
+
+  }
+
+
+}
+
+void loop()
+{
+    
+  lv_timer_handler(); /* let the GUI do its work */
+  delay(5);
+}
